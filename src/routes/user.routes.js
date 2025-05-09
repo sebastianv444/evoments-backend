5bf59873708b1fbab56c9efa23edb867da010e5e
sebastian <sebastian12verositi@gmail.com>
2025-05-09 18:26:11 +0200

    Autenticacion acabada con clerk

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,8 @@
 import { Router } from "express";
-import { clerkProtect } from "../middlewares/withClerk.js";
 import * as controller from "../controllers/user.controller.js";
 const router = Router();
 
-router.post("/sync", clerkProtect, controller.syncUserHandler);
+router.post("/sync" ,controller.syncUserHandler);
 router.get("/prueba", (req, res) => res.status(200).json({ bien: true }));
 
 export default router;
